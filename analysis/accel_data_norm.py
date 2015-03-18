293b87b2a981eb41b902b22a651a3bbb3ea59021
Zack Zhu <zackzhu@gmail.com>
2015-03-18 15:00:16 +0100

    bug fixing for database creation

--- analysis/accel_data_norm.py
+++ analysis/accel_data_norm.py
@@ -39,11 +39,12 @@ def is_timestamp_valid(timestamp):
 	return True
 
 def is_accel_data_valid(accel_data):
-	return not np.any(np.abs(accel_data)>4000)
+	return not np.any(np.abs(accel_data)>8000)
 
 def is_new_phone_format(phone_file):
 	with open(phone_file, "rb") as infile:
-		read_success, rec = read_packet(infile, "test", True) # Read with new format
+		
+		read_success, rec = read_packet(infile, "test", is_phone = True, new_format = True) # Read with new format
 
 		if read_success and rec != {}:
 			print(phone_file + "-> new format")
@@ -52,7 +53,7 @@ def is_new_phone_format(phone_file):
 			print(phone_file + "-> old format")
 			return False
 
-def read_packet(infile, user, is_new_format):
+def read_packet(infile, user, is_phone, new_format):
 	'''Read and consume 8 (timestamp) + 25*3*2 (25 triples of x,y,z, each of type short).
 	Also check if the timestamp is valid and the accelerometer datas are between [-4k,+4k].
 
@@ -64,20 +65,29 @@ def read_packet(infile, user, is_new_format):
 					 or accel data does not make sense.
 		(false, {}): read failed. It means the end of file.
 	'''
-	# Read timestamp
-	timestamp = np.fromfile(infile, dtype=">Q", count=1) #uint64_t in big endian
+	
 
-	# Check if end of file
+	# Read timestamp
+	if is_phone:
+		timestamp = np.fromfile(infile, dtype=">Q", count=1) #uint64_t in big endian
+	else:
+		timestamp = np.fromfile(infile, dtype="<Q", count=1) #uint64_t in little endian
+	
+	# Check whether timestamp was successfully read
 	if len(timestamp) != 1:
-		print ("packet read fail: timestamp returned []")
+		print ("timestamp returned [], exiting")
 		return False, {}
 
 	accel_data=[]
 	try:
-		if is_new_format:
-			accel_data = np.fromfile(infile, dtype=">h", count=25*3).reshape(-1,3)  # 75 uint8_t in big endian
+		if is_phone:
+			if new_format:
+				accel_data = np.fromfile(infile, dtype=">h", count=25*3).reshape(-1,3)  # 75 uint8_t in big endian
+			else:
+				pdb.set_trace()
+				accel_data = np.fromfile(infile, dtype=">h", count=3).reshape(-1,3)  # 75 uint8_t in big endian
 		else:
-			accel_data = np.fromfile(infile, dtype=">h", count=3).reshape(-1,3)  # 3 uint8_t in big endian
+			accel_data = np.fromfile(infile, dtype="<h", count=25*3).reshape(-1,3)  # 75 uint8_t in little endian
 	except ValueError:
 		# End of the file and accel_data is []
 		print ("packet read fail: accel_data returned []")
@@ -85,6 +95,9 @@ def read_packet(infile, user, is_new_format):
 
 	# Check validity of the data
 	if not is_timestamp_valid(timestamp) or not is_accel_data_valid(accel_data):
+		print ("packet content was invalid, returning empty record")
+		print (timestamp)
+		print (accel_data)
 		return True, {}
 
 	# Form record
@@ -99,12 +112,6 @@ def read_packet(infile, user, is_new_format):
 	return True, rec
 
 def stitch_phone_data(users = all_users):
-	#TODO: remove these counters
-	num_old_file = 0
-	num_new_file = 0
-	num_total_file = 0
-
-	num_entries = 0
 
 	for user in users:
 		phone_files = glob.glob(raw_data_directory+user+"/phoneAccel_"+user+"*")
@@ -114,52 +121,39 @@ def stitch_phone_data(users = all_users):
 		for phone_file in phone_files:	
 
 			records = []
-			num_total_file+=1
+			
 
-			# Need to put it in to a local boolean because is_new_phone_format has side effect:
-			# it opens the file and consumes the first packet of the data
-			is_new_format=False
-			if is_new_phone_format(phone_file):
-				is_new_format=True
+			if not is_new_phone_format(phone_file): #skip file if old format
+				continue
 
 			# Reopen the file and start from the begining
 			with open(phone_file, "rb") as infile:
 				while True:
-					if is_new_format:
-						read_success, rec = read_packet(infile, user, True) # Read with new format
-					else:
-						read_success, rec = read_packet(infile, user, False) # Read with old format
-					if not read_success:
+
+					read_success, rec = read_packet(infile, user, is_phone = True, new_format = True) # Read with new format
+					
+					if not read_success: #False when end of file OR data type is not right
 						break
 
-					if(rec == {}):
+					if (rec == {}): # Read was fine but packet content was invalid
 						continue
 
 					records.append(rec)
-					num_entries+=len(rec["x"])
+					
 
 				# Insert all records for this file
 				if records == []:
 					continue
-				if is_new_format:
-					num_new_file+=1
-				else:
-					num_old_file+=1
 
-			#db.phone_accel.insert(records)
+
+			db.phone_accel.insert(records)
 			
-			# # Move the inserted documents into processed subfolder
-			# directory = raw_data_directory+user+"/processed/"
-			# if not os.path.exists(directory):
-			# 	os.makedirs(directory)
-			# shutil.move(phone_file, directory)
+			# Move the inserted documents into processed subfolder
+			directory = raw_data_directory+user+"/processed/"
+			if not os.path.exists(directory):
+				os.makedirs(directory)
+			shutil.move(phone_file, directory)
 
-	print(num_new_file)
-	print(num_old_file)
-	print(num_total_file)
-
-	print("In total we get this many entires:")
-	print(num_entries)
 
 def stitch_pebble_data(users = all_users):
 	for user in users:
@@ -169,61 +163,34 @@ def stitch_pebble_data(users = all_users):
 		for pebble_file in pebble_files:
 			with open(pebble_file, "rb") as infile:
 				records = []
-				print ("begin reading new file: " + str(infile))
+				print ("\n\nbegin reading new file: " + str(infile))
 				while True:
 
-					read_success, rec = read_packet(infile, user, True)
+					read_success, rec = read_packet(infile, user, is_phone = False, new_format = True)
 
 					if not read_success:
 						break
 
 					if (rec == {}):
 						continue
+
+
 					records.append(rec)
 
 				# go to next file if no records are in this one
 				if len(records) == 0:
 					continue
-
-#				db.pebble_accel.insert(records)
-				# # Move the inserted documents into processed subfolder
-				# directory = raw_data_directory+user+"/processed/"
-				# if not os.path.exists(directory):
-				# 	os.makedirs(directory)
-				# shutil.move(pebble_file, directory)
-
-					# # Read data
-					# timestamp = np.fromfile(infile, dtype="<Q", count=1) #uint64_t in little endian
-
-					# if len(timestamp) != 1:
-					# 	break
-
-					# # TODO: modify filter to check between beginning of this year and time.time()
-					# if is_timestamp_valid(timestamp): # error in file, break out
-					# 	break
-
-					# accel_data = np.fromfile(infile, dtype="<h", count=25*3).reshape(-1,3)  # 75 uint8_t in little endian
-
-					# if np.any(np.abs(accel_data)>4000): # error in file, break out
-					# 	break
-
-					# # Form record
-					# rec = { 
-					# 	"user": user,
-					# 	"timestamp":float(timestamp[0]/1000.0), 
-					# 	"x":accel_data[:,0].tolist(),
-					# 	"y":accel_data[:,1].tolist(),
-					# 	"z":accel_data[:,2].tolist()
-					# 	}
-					# records.append(rec)
-
-				# # Insert all records for this file
-				# if records == []:
-				# 	continue
 				
-			
+				print ("inserting %d records" % len(records))
+				db.pebble_accel.insert(records)
+				# Move the inserted documents into processed subfolder
+				directory = raw_data_directory+user+"/processed/"
+				if not os.path.exists(directory):
+					os.makedirs(directory)
+				shutil.move(pebble_file, directory)		
 				
 				
 
 if __name__ == "__main__":
+	#stitch_pebble_data()
 	stitch_phone_data()
